874c8e9285df87f9a4b698c2235df395b2836c08
Riley Cleary <rileykeycleary@yahoo.com>
2018-07-12 16:29:56 -0700

    Adds module start rack

--- lib/rc_rulers.rb
+++ lib/rc_rulers.rb
@@ -1,5 +1,10 @@
 require "rc_rulers/version"
 
 module RcRulers
-  # Your code goes here...
+  class Application
+    def call(env)
+      [200, {'Content-Type' => 'text/html'},
+        ["Hello from my Gem"]]
+    end
+  end
 end
