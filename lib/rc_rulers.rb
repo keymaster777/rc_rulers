9115370727f9e10676978cee1b6ff1b95f2564db
Riley Cleary <rileykeycleary@yahoo.com>
2018-07-12 17:30:59 -0700

    adds error log

--- lib/rc_rulers.rb
+++ lib/rc_rulers.rb
@@ -3,6 +3,7 @@ require "rc_rulers/version"
 module RcRulers
   class Application
     def call(env)
+      'echo debug > debug.txt';
       [200, {'Content-Type' => 'text/html'},
         ["Hello from my Gem"]]
     end
