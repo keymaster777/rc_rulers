ee65b1d8c5d55da05c4537c1d74e7549b04d2de2
Riley Cleary <rileykeycleary@yahoo.com>
2018-07-12 18:36:53 -0700

    Deleting packaged gems

--- lib/rc_rulers.rb
+++ lib/rc_rulers.rb
@@ -1,11 +1,12 @@
 require "rc_rulers/version"
+require "rc_rulers/array"
 
 module RcRulers
   class Application
     def call(env)
       'echo debug > debug.txt';
       [200, {'Content-Type' => 'text/html'},
-        ["Hello from my Gem"]]
+        ["Hello from my Gem 1"]]
     end
   end
 end
