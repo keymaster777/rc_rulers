ec33ce3d8a047128d773c89e125c7f447d367244
Riley Cleary <rileykeycleary@yahoo.com>
2018-07-12 22:08:27 -0700

    Initializes testing suite

--- lib/rc_rulers.rb
+++ lib/rc_rulers.rb
@@ -4,7 +4,7 @@ require "rc_rulers/array"
 module RcRulers
   class Application
     def call(env)
-      'echo debug > debug.txt';
+      `echo debug > debug.txt`
       [200, {'Content-Type' => 'text/html'},
         ["Hello from my Gem 2"]]
     end
